e95271299a4e2d481e641e4352eb79ed08899044
yoannfelix <67986128+yoannfelix@users.noreply.github.com>
2021-02-16 17:06:04 -0500

    Actor setup and character sheet
    Actors have been setup. The character sheet are slowly being set up. Changed to .hbs instead of .html so the weapon sheet is no longer working for some reason.

--- witcherrpg.js
+++ witcherrpg.js
@@ -1,9 +1,15 @@
-import witcheritemSheet from "./module/sheets/witcheritemSheet.js";
+import {witcherrpg} from "./module/config.js";
+import witcherItemSheet from "./module/sheets/witcherItemSheet.js";
+import witcherCharacterSheet from "./module/sheets/witcherPCSheet.js";
 
 Hooks.once("init", function(){
     console.log("witcherrpg | Initializing The Witcher RPG System");
+   
+    CONFIG.witcherrpg = witcherrpg;
 
-    Items.unregiterSheet("core", Itemsheet);
-    Items.registerSheet("witcherrpg", witcherItemSheet, { MakeDefault: true});
+    Items.unregisterSheet("core", ItemSheet);
+    Items.registerSheet("witcherrpg", witcherItemSheet, { types: ["weapon"], MakeDefault: true, label: "WitcherRPG.SheetClassItem"});
 
+    Actors.unregisterSheet("core", ActorSheet);
+    Actors.registerSheet("witcherrpg", witcherCharacterSheet, { types: ["weapon"], MakeDefault: true, label: "WitcherRPG.SheetClassItem"});
 });
