77d78932500efd8fe7eb14e02420d8e155a73a94
yoannfelix <67986128+yoannfelix@users.noreply.github.com>
2021-02-28 17:22:55 -0500

    Added Character Sheet
    Basic character info and character name have been added.
    
    Weapon sheet's design shows up as an item rather than pure stats like the book.

--- module/config.js
+++ module/config.js
@@ -5,17 +5,50 @@ witcherrpg.coreStats = {
 
 }
 
+witcherrpg.keySkills ={
+    definingSkill: "witcherrpg.keySkills.definingSkill",
+    professionalAbility: "witcherrpg.keySkills.professionalAbility"
+}
 
-witcherrpg.swordTypes = {
+witcherrpg.coreDetails = {
+    charname: "witcherrpg.coreDetails.charName",
+    biography: "witcherrpg.coreDetails.biography",
+    racetypes: "witcherrpg.coreDetails.racetypes",
+    chargender: "witcherrpg.coreDetails.chargender",   
+    charage: "witcherrpg.coreDetails.charage",
+    profession: "witcherrpg.coreDetails.profession",
+    raceperks00: "witcherrpg.coreDetails.racePerk00",
+    raceperks01: "witcherrpg.coreDetails.racePerk01",
+    raceperks02: "witcherrpg.coreDetails.racePerk02",
+    raceperks03: "witcherrpg.coreDetails.racePerk03"
+}
+
+witcherrpg.weaponAtt = {
     none: "",
-    swordName: "witcherrpg.swordStats.swordName",
-    swordType: "witcherrpg.swordStats.swordType",
-    swordWA: "witcherrpg.swordStats.swordWA",
-    swordRel: "witcherrpg.swordStats.swordRel",
-    swordHand: "witcherrpg.swordStats.swordHand",
-    swordRange: "witcherrpg.swordStats.swordRange",
-    swordEffect: "witcherrpg.swordStats.swordEffect",
-    swordConc: "witcherrpg.swordStats.swordConc",
-    swordEN: "witcherrpg.swordStats.swordEN",
-    swordWeight: "witcherrpg.swordStats.swordWeight"
+    weaponName: "witcherrpg.weaponAtt.weaponName",
+    weaponType: "witcherrpg.weaponAtt.weaponType",
+    weaponAcc: "witcherrpg.weaponAtt.weaponAcc",
+    weaponDMG:"witcherrpg.weaponAtt.weaponDMG",
+    weaponDMGType: "witcherrpg.weaponAtt.weaponDMGType",
+    weaponRel: "witcherrpg.weaponAtt.weaponRel",
+    weaponHand: "witcherrpg.weaponAtt.weaponHand",
+    weaponRange: "witcherrpg.weaponAtt.weaponRange",
+    weaponEffect: "witcherrpg.weaponAtt.weaponEffect",
+    weaponConc: "witcherrpg.weaponAtt.weaponConc",
+    weaponCost: "witcherrpg.weaponAtt.weaponCost",
+    weaponEN: "witcherrpg.weaponAtt.weaponEN",
+    weaponWeight: "witcherrpg.weaponAtt.weaponWeight"
+}
+
+witcherrpg.weaponList ={
+    none: "",
+    sword: "witcherrpg.weaponList.sword",
+    smallBlade: "witcherrpg.weaponList.smallBlade",
+    axe: "witcherrpg.weaponList.axe",
+    bludgeon: "witcherrpg.weaponList.bludgeon",
+    poleArm: "witcherrpg.weaponList.poleArm",
+    staff: "witcherrpg.weaponList.staff",
+    thrownWeapon: "witcherrpg.weaponList.thrownWeapon",
+    bow: "witcherrpg.weaponList.bow",
+    crossBow: "witcherrpg.weaponList.crossBow"
 }
