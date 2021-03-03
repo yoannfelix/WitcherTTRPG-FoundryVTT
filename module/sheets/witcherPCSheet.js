f7f8193234ec66c5308e9821676dab5f3c39b8b8
yoannfelix <67986128+yoannfelix@users.noreply.github.com>
2021-03-03 18:55:58 -0500

    Primary Weapon Array has been added to PC-Sheet

--- module/sheets/witcherPCSheet.js
+++ module/sheets/witcherPCSheet.js
@@ -6,11 +6,10 @@ export default class witcherPCSheet extends ActorSheet {
 		});
 	}
 
-	GetData(){
+	getData(){
 		const data = super.getData();
 		data.config = CONFIG.witcherrpg;
 		data.weapons= data.items.filter(function (item){ return item.type == "weapon"});
 		return data;
 	}
 }
-
